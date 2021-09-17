e22df5af29c838ebc9b3142d718b469dd36cc0a3
ErjillDeVera <erjilldevera@icloud.com>
2021-09-17 14:01:45 +0100

    Created a dbs connection locally

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
+
 
 app.listen(3000, function(){
   console.log("Server started on port 3000");
